b2a760582f95be823e1c5364218b9b702267bd3e
Ilya <sayex9001@mail.ru>
2022-09-22 15:01:14 +0300

    Done Task002

---
+++ Task_002/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите ваше число: ");
+string input = Console.ReadLine();
+    if (input.Length > 2)
+        Console.WriteLine("{0}->{1}",input, input[2]);
+    else
+        Console.WriteLine("Число не состоит из трех символов");
