a5bcf4a9a79763c590830b183b834666ae7863d3
Ilya <sayex9001@mail.ru>
2022-09-22 15:10:50 +0300

    Done Task003

---
+++ Task_003/Program.cs
@@ -0,0 +1,12 @@
+﻿// По хорошему тут все дни недели указать, что было более корректно, но тогда код будет конских размеров :D
+int a;
+Console.WriteLine("Введите нужный день недели: ");
+a = Convert.ToInt32(Console.ReadLine());
+    if (a == 6)
+        Console.WriteLine("Это суббота!Выходной!");
+    if (a == 7)
+        Console.WriteLine("Это воскресенье!Выходной!");
+    if (a > 7)
+        Console.WriteLine("Неккоректное значение!");
+    if (a < 6)
+        Console.WriteLine("Это не выходной!");    
